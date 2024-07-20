30df91d5dfca805a675ab3cede647e7224f0108c
Jamie Taylor <Jamie.Taylor@pobox.com>
2024-07-19 21:19:47 -0400

    Initial release

---
+++ HoldToProcessGeodes/ModEntry.cs
@@ -0,0 +1,49 @@
+﻿// Copyright 2024 Jamie Taylor
+namespace HoldToProcessGeodes;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+using StardewValley;
+using StardewValley.Menus;
+
+public class ModEntry : Mod {
+    private ButtonState prevState = ButtonState.Released;
+    private bool clickStartedInGeodeSpot = false;
+
+    public override void Entry(IModHelper helper) {
+        helper.Events.GameLoop.UpdateTicked += OnUpdateTicked;
+    }
+
+    private void OnUpdateTicked(object? sender, UpdateTickedEventArgs e) {
+        var newState = Game1.input.GetMouseState().LeftButton;
+        if (Game1.activeClickableMenu is GeodeMenu menu) {
+            if (newState == ButtonState.Pressed) {
+                int mouseX = Game1.getMouseX(true);
+                int mouseY = Game1.getMouseY(true);
+                if (prevState == ButtonState.Released) {
+                    // new click; check whether it's in the geode area
+                    clickStartedInGeodeSpot = menu.geodeSpot.containsPoint(mouseX, mouseY);
+                }
+                if (clickStartedInGeodeSpot 
+                    && menu.geodeSpot.containsPoint(mouseX, mouseY)
+                    && menu.geodeAnimationTimer <= 0
+                    && menu.alertTimer <= 0
+                    && menu.wiggleWordsTimer <= 0
+                    && Game1.dayTimeMoneyBox.moneyShakeTimer <= 0)
+                {
+                    menu.receiveLeftClick(mouseX, mouseY);
+                }
+            } else {
+                clickStartedInGeodeSpot = false;
+            }
+        } else {
+            clickStartedInGeodeSpot = false;
+        }
+        prevState = newState;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Stardew Valley Hold to Process Geodes
+
+A Stardew Valley [SMAPI](https://smapi.io) mod that repeatedly processes
+geodes while the left mouse button is held down.
+
+You can find this mod on [Nexus Mods](https://www.nexusmods.com/stardewvalley/mods/26189)
+if you prefer.
+
+Use by itself or combine with [Fast Animations](https://www.nexusmods.com/stardewvalley/mods/1089) for
+super-speedy geode cracking!
+
+----
+
+Inspired by [Hold to Break Geodes](https://www.nexusmods.com/stardewvalley/mods/1689), which
+is [no longer maintained](https://danvolchek.com/projects/stardew-valley-mods/#the-end-of-an-era)
+and does not have an open source license.  If it did I would have forked it instead of doing a
+from-scratch rewrite. 
